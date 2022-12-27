b178ad786c71a75c76ea652380ff9345813fa8a0
ㅤ <83069165+0verview@users.noreply.github.com>
2022-12-27 18:22:49 +0100

    Create cifar10_rec.py

---
+++ cifar10_rec.py
@@ -0,0 +1,35 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.datasets import cifar10
+
+# Load the CIFAR10 dataset
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+# Convert the labels to one-hot encoded vectors
+y_train = tf.keras.utils.to_categorical(y_train, num_classes=10)
+y_test = tf.keras.utils.to_categorical(y_test, num_classes=10)
+
+# Define the model architecture
+input_shape = (32, 32, 3)
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(10, activation='softmax'))
+
+# Compile the model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Train the model on the federated learning system
+model.fit(x_train, y_train, epochs=10)
+
+# Select a sample from the training set to reconstruct
+sample_idx = 0
+sample = x_train[sample_idx]
+
+# Use the model to reconstruct the sample
+reconstructed_sample = model.predict(np.expand_dims(sample, axis=0))
+
+# Compare the original and reconstructed samples
+print(f'Original sample: {sample}')
+print(f'Reconstructed sample: {reconstructed_sample}')
